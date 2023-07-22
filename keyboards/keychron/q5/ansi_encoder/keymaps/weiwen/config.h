e0123689bc7c46fbad64b2e24fbf71a34598f679
Wei Wen Goh <goweiwen@gmail.com>
2023-07-23 00:32:26 +0800

    Add custom keymap for Keychron Q5

---
+++ keyboards/keychron/q5/ansi_encoder/keymaps/weiwen/config.h
@@ -0,0 +1,7 @@
+/* QMK Apple Fn Key: https://gist.github.com/fauxpark/010dcf5d6377c3a71ac98ce37414c6c4 */
+#define APPLE_FN_ENABLE
+#undef VENDOR_ID
+#define VENDOR_ID 0x05AC
+#undef PRODUCT_ID
+#define PRODUCT_ID 0x0267
+
